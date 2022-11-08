84732acdd4ce43e035e430b1fef621bc940bc96c
MazkaAndrii <andriy.mazka@gmail.com>
2022-11-08 18:44:43 +0200

    task_2_fix

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const listItems = ingredients.map(ingredient => {
+   const listItem = document.createElement('li');
+   listItem.textContent = [ingredient];
+   listItem.classList.add('item');
+   return listItem;
+});
+
+console.log(listItems);
+
+const listEl = document.querySelector('ul#ingredients');
+listEl.append(...listItems);
+
+console.log(listEl);
